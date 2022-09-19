343d6442d756272d3d14abf140a180116b0799dc
AlekBentao <113909760+AlekBentao@users.noreply.github.com>
2022-09-19 10:31:02 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("A GALERA DO BENTÃO É FERA!")
+st.title("É MEC OU NÃO É?!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("UOOÔ Ô Ô Ô")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("O ALEK É O TERROR!!!!")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("Paiva é log!")
